dc809904043bc4fb4cadaf8b13c73f2874e38b7c
John <johntheherrera@gmail.com>
2024-10-29 15:35:44 -0700

    created GPS shit

--- func/GPS.py
+++ func/GPS.py
@@ -3,15 +3,33 @@ import time
 import string
 import pynmea2
 
-while True:
-	port="/dev/ttyAMA0"
+def calibrate_GPS():
+	
+	global poty, ser, dataout, newdata
+	
+	port="/dev/ttyS0"
 	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
 	dataout = pynmea2.NMEAStreamReader()
 	newdata=ser.readline()
-	
-	if newdata[0:6] == "GPRMC":
-		newmsg=pynmea2.parse(newdata)
-		lat=newmsg.latitude
-		lng=newmsg.longitude
-		gps = "Latitude=" + str(lat) + " Longitude=" + str(lng)
+
+def get_GPS():
+	# x[0] = lat
+	# x[1] = lng
+	newmsg=pynmea2.parse(newdata.decode("utf-8"))
+	lat=newmsg.latitude
+	lng=newmsg.longitude
+	return [lat,lng]
+
+
+""" Main function for debugging """
+		
+def main():
+	calibrate_GPS()
+	while True:
+		gps_data = get_GPS()
+		gps = "Latitude=  " + str(gps_data[0])+ "  and Longitude=" + str(gps_data[1])
 		print(gps)
+		time.sleep(1)
+		 
+if __name__ == "__main__":
+    main()
