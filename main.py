9d297c0fd6ca33ef8916350d58ead5fa00c7280a
John <johntheherrera@gmail.com>
2024-11-08 12:40:36 -0800

    creating main file

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 from func import *
+import time
 
-print("Hello world")
+def main():
+    print("Hello World")
+    
+    n = 0
+    begin = time.time()
+    
+    while True:
+        
+        data = {"n": n, "timestamp": time.time() - begin, "altitude": 0, "latitude": 0, "longitude": 0, "temperature": 0, "humidity": 0, "pressure": 0, "acceleration": 0, "gyroscope": 0, "battery": 0}
+        print(data)
+        n += 1
+        time.sleep(0.1)
+ 
+ 
+ 
+		 
+if __name__ == "__main__":
+    main()
