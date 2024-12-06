98bf72b55a0bb20f86db5947fab70b7b90e834d1
John <johntheherrera@gmail.com>
2024-12-05 18:26:27 -0800

    s

---
+++ main_141.py
@@ -0,0 +1,26 @@
+from func import bmp
+import time
+import csv
+import threading
+
+"""
+Pins:
+
+BMP280 -> PI4:
+SCK -> 5
+SDI -> 3
+
+GPS6MV2 -> PI4:
+TX -> 10
+
+"""
+
+#--------------------Globals Vars--------------------
+
+#--------------------Functions-----------------------
+
+def main():
+    print("Start!")
+    
+if __name__ == "__main__":
+    main()
