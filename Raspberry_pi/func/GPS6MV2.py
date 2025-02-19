2629378395c827ddf43c28ee8f33560608f443b7
John <johntheherrera@gmail.com>
2025-02-18 22:04:56 -0800

    adding serial shit

--- Raspberry_pi/func/GPS6MV2.py
+++ Raspberry_pi/func/GPS6MV2.py
@@ -1,8 +1,10 @@
 import serial
+import time
 
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
 
 while True:
     data = ser.readline().decode('utf-8', errors='ignore')
-    if data.startswith('$GPGGA'):
-        print(data)
+    print(data)
+    time.sleep(0.1)  
+    
