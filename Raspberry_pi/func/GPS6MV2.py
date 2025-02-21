fdfe4180799afaaa6bf31d317b5235ca72bf3374
John <johntheherrera@gmail.com>
2025-02-20 23:48:46 -0800

    ahhhhhhhhhhhhhhhhhhhhhhhh

--- Raspberry_pi/func/GPS6MV2.py
+++ Raspberry_pi/func/GPS6MV2.py
@@ -3,22 +3,9 @@ import time
 import string
 import pynmea2
 
-def calibrate_GPS():
-	
-	global ser, dataout, newdata
-	try:
-		port="/dev/ttyS0"
-		ser=serial.Serial(port, baudrate=9600, timeout=0.5)
-		dataout = pynmea2.NMEAStreamReader()
-		newdata=ser.readline()
-	except Exception as e:
-		print("Error: ", e)
-
 def get_GPS():
 	# x[0] = lat
 	# x[1] = lng
-	
-	global ser, dataout, newdata
  
 	try:
 		port = "/dev/ttyS0"
@@ -27,6 +14,7 @@ def get_GPS():
 		newmsg = pynmea2.parse(newdata.decode("utf-8"))
 		lat = newmsg.latitude
 		lng = newmsg.longitude
+		ser.close
 	except Exception as e:
 		print("Error: ", e)
 		lat = -1
@@ -38,7 +26,6 @@ def get_GPS():
 """ Main function for debugging """
 		
 def main():
-	calibrate_GPS()
 	while True:
 		gps_data = get_GPS()
 		gps = "Latitude=  " + str(gps_data[0])+ "  and Longitude=" + str(gps_data[1])
